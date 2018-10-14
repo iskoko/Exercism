836f2b1ef170e9328e01dfadd3254f39f1bf7b77
iskoko <skoko.ivona@gmail.com>
2018-10-14 02:43:57 +0200

    Add rna-transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,18 @@
+const dnaToRna = {
+    G : 'C',
+    C : 'G',
+    T : 'A',
+    A : 'U'
+}
+
+const toRna = (dnaStrand) => {
+    if (/[^GCTA]/gi.test(dnaStrand)) {
+        throw new Error('Invalid input DNA.');
+    }
+    const dnaStrandUC = dnaStrand.toUpperCase();
+    return dnaStrandUC.replace(/[GCTA]/g, rna => {
+        return dnaToRna[rna];
+    });
+}
+
+export {toRna};
